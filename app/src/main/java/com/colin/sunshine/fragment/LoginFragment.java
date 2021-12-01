5f8afc8f1f21afab5bcf0c42790780640ff4a5c1
gsrkuang <gsrkuang@163.com>
2021-12-01 21:13:16 +0800

    LoginFragment

---
+++ app/src/main/java/com/colin/sunshine/fragment/LoginFragment.java
@@ -0,0 +1,11 @@
+package com.colin.sunshine.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Date:2021-12-01
+ * Time:21:06
+ * author:colin
+ */
+public class LoginFragment extends Fragment {
+}
