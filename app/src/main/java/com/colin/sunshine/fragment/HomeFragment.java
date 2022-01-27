55dd757c352ec0deb020ea7015db22ca3b8f39a1
gsrkuang <gsrkuang@163.com>
2022-01-27 19:15:40 +0800

    修复刷新数据bug

--- app/src/main/java/com/colin/sunshine/fragment/HomeFragment.java
+++ app/src/main/java/com/colin/sunshine/fragment/HomeFragment.java
@@ -82,6 +82,7 @@ public class HomeFragment extends Fragment {
                     @Override
                     public void run() {
                         swipeRefreshLayout.setRefreshing(false);
+                        page = 1;
                         getHttpData();
                     }
                 });
